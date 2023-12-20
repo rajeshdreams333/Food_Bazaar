da0e2fae9c98ea4e5070b5a7779ae36dd75236a4
Rajeshdreams333 <rajeshdreams333@gmail.com>
2023-12-21 00:14:42 +0530

    add readme

--- App.js
+++ App.js
@@ -1,3 +1,14 @@
-const heading=React.createElement("h1",{id:"heading"},"Hello World!!!🚀");
-const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+// const heading=React.createElement("h1",{id:"heading"},"Hello World!!!🚀");
+// const root=ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am H1 tag..."),
+    React.createElement("h2", {}, "I am H2 tag..."),
+  ])
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
