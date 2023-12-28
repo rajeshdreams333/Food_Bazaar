e01bed025d2ace3c639e4a024c00cb68b4698aea
Rajeshdreams333 <rajeshdreams333@gmail.com>
2023-12-28 22:50:10 +0530

    4th video 42:57

--- App.js
+++ App.js
@@ -1,12 +1,51 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am H1 tag..."),
-    React.createElement("h2", {}, "I am H2 tag..."),
-  ])
-);
+const Header = () => {
+  return (
+    <div className="header">
+      <div>
+        <img
+          src="https://png.pngtree.com/png-vector/20220527/ourmid/pngtree-food-logo-png-image_4743675.png"
+          alt="logo not found"
+          className="logo"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+const RestoCard=()=>{
+  return(
+    <div className="restocard" style={{backgroundColor:"#f0f0f0"}}>
+    <h1>Meghana Foods</h1>
+    </div>
+  )
+}
+const Body=()=>{
+  return(
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestoCard/>
+      </div>
+    </div>
+  )
+}
+const AppLout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body/>
+    </div>
+  );
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+
+root.render(<AppLout />);
