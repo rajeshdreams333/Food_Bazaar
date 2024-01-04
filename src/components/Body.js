3080ee2d6e9d2402533611b21f8995f9f9c5be13
Rajeshdreams333 <rajeshdreams333@gmail.com>
2024-01-05 02:02:26 +0530

    6th video 1hr:30min

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,28 @@
 import RestoCard from "./Restocard";
-import resObj from "../utils/mockData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 const Body = () => {
-  const [restoList, setRestoList] = useState(resObj);
+  
+  const [restoList, setRestoList] = useState([]);
+
+  useEffect(()=>{
+    fetchData();
+  },[])
+  const fetchData=async ()=>{
+    const data=await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.8089863&lng=83.3455422&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
+    const json=await data.json()
+    console.log(json);
+    setRestoList(json?.data?.cards[3]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+  }
   return (
     <div className="body">
       <div className="filter">
+        <div className="search">
+              <input type="text" className="search-box" />
+              <button className="search-btn">Search</button>
+        </div>
         <button className="filter-btn" onClick={()=>{
           const filterResto=restoList.filter(
-            (res)=>res.info.avgRating===4.1
+            (res)=>res.info.avgRating>4
           );
           setRestoList(filterResto)
         }}>Top Rated Resto</button>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,30 @@
+import { useState } from "react";
 import { CDN_LOGO } from "../utils/constants";
 const Header = () => {
-    return (
-      <div className="header">
-        <div>
-          <img
-            src={CDN_LOGO}
-            alt="logo not found"
-            className="logo"
-          />
-        </div>
-        <div className="nav-items">
-          <ul>
-            <li>Home</li>
-            <li>About Us</li>
-            <li>Contact Us</li>
-            <li>Cart</li>
-          </ul>
-        </div>
+  const [btn, setBtn] = useState("Login");
+  return (
+    <div className="header">
+      <div>
+        <img src={CDN_LOGO} alt="logo not found" className="logo" />
       </div>
-    );
-  };
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+          <button
+            className="login"
+            onClick={() => {
+              btn === "Login" ? setBtn("Logout") : setBtn("Login");
+            }}
+          >
+            {btn}
+          </button>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
