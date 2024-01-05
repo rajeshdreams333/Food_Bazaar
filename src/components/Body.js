7532f616c3e58b523d83cf5366c2b6c55d1175c1
Rajeshdreams333 <rajeshdreams333@gmail.com>
2024-01-05 23:58:55 +0530

    6th video completed

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,35 +2,70 @@ import RestoCard from "./Restocard";
 import { useEffect, useState } from "react";
 import Shimmer from "./Shimmer";
 const Body = () => {
-  
   const [restoList, setRestoList] = useState([]);
+  const [filterResto, setFilterResto] = useState([]);
 
-  useEffect(()=>{
+  const [searchvalue, setSearchvalue] = useState([""]);
+
+  useEffect(() => {
     fetchData();
-  },[])
-  const fetchData=async ()=>{
-    const data=await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.8089863&lng=83.3455422&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
-    const json=await data.json()
-    console.log(json);
-    setRestoList(json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants||json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
-  }
+  }, []);
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.8089863&lng=83.3455422&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    // console.log(json);
+    setRestoList(
+      json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants ||
+        json?.data?.cards[3]?.card?.card?.gridElements?.infoWithStyle
+          ?.restaurants ||
+        json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
+          ?.restaurants
+    );
+    setFilterResto(
+      json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants ||
+      json?.data?.cards[3]?.card?.card?.gridElements?.infoWithStyle?.restaurants ||
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
 
-  return restoList.length===0 ?<Shimmer/>: (
+  return restoList == 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
       <div className="filter">
         <div className="search">
-              <input type="text" className="search-box" />
-              <button className="search-btn">Search</button>
+          <input type="text"
+          className="search-box"
+          value={searchvalue}
+          onChange={(e)=>{
+            setSearchvalue(e.target.value)
+          }} />
+          <button className="search-btn" onClick={()=>{
+            const filterdata=restoList.filter((res)=>
+             res.info.name.toLowerCase().includes(searchvalue.toLowerCase())
+            );
+            setFilterResto(filterdata)
+          }}>
+            Search
+          </button>
         </div>
-        <button className="filter-btn" onClick={()=>{
-          const filterResto=restoList.filter(
-            (res)=>res.info.avgRating>4
-          );
-          setRestoList(filterResto)
-        }}>Top Rated Resto</button>
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filterResto = restoList.filter(
+              (res) => res.info.avgRating > 4
+            );
+            setRestoList(filterResto);
+          }}
+        >
+          Top Rated Resto
+        </button>
       </div>
       <div className="res-container">
-        {restoList.map((resto) => (
+        {filterResto.map((resto) => (
           <RestoCard key={resto.info.id} resData={resto} />
         ))}
       </div>
